49ec9638b28f190c5ecc114f41152145bd43a0c0
Владислав Прекель <79232809414@yandex.ru>
2016-12-07 23:12:02 +0700

    init

---
+++ main.ts
@@ -0,0 +1,18 @@
+﻿function mix(str: string): string {
+	var p = str;
+	for (var i = 1; i < str.length - 1; i++) {
+		if (str[i] == p[i]) {
+			for (var j = 1; j < str.length - 1; j++) {
+				if (p[j] != str[i] && str[j] != p[i]) {
+					var temp = p[i];
+					p = p.substring(0, i) + p[j] + p.substring(i + 1);
+					p = p.substring(0, j) + temp + p.substring(j + 1);
+				}
+			}
+		}
+	}
+	return p;
+}
+
+let h = /[а-я]{3,}/gi; //шаблон: слова из букв русского алфавита длиннее 3 букв
+document.body.innerHTML = document.body.innerHTML.replace(h, mix);
